57781f90e52d534c9dfa8bed9f7f93c3a5e40b69
Mahi <Mahi@192.168.0.3>
2016-06-30 18:55:45 +0530

    emial reg exp updated

--- FormValidation/WebContent/js/formvalid.js
+++ FormValidation/WebContent/js/formvalid.js
@@ -85,7 +85,7 @@ return true;
 }
 // Function that checks whether an user entered valid email address or not and displays alert message on wrong email address format.
 function emailValidation(inputtext, alertMsg) {
-var emailExp = /^[w-.+]+@[a-zA-Z0-9.-]+.[a-zA-z0-9]{2,4}$/;
+var emailExp = "/^[w-.+]+@[a-zA-Z0-9.-]+.[a-zA-z0-9]{2,4}$/";
 if (inputtext.value.match(emailExp)) {
 return true;
 } else {
